497fbf7ee626f690a7f9c90b7eca0800374222d0
coderpwh <plmoknpwh@163.com>
2025-09-08 21:41:01 +0800

    fix

--- ui/navigation.go
+++ ui/navigation.go
@@ -12,7 +12,7 @@ func CreateSidebar(window fyne.Window, contentContainer *fyne.Container) *contai
 		container.NewTabItem("我的内容", MyContentContent(contentContainer)),
 		container.NewTabItem("上传", UploadContent(contentContainer)),
 		container.NewTabItem("历史与收藏", HistoryAndFavoritesContent()),
-		container.NewTabItem("设置", SettingsContent()),
+		container.NewTabItem("设置", SettingsContent(window)),
 	)
 
 	tabs.SetTabLocation(container.TabLocationLeading)
